0721f15c79d1b694148e368bdd8cf231f558a619
rahtz <rahtz@4830b8e0-8a90-4283-b7f1-a7364ff6f0ab>
2012-06-16 12:12:31 +0000

    add support for TCP and normalize name
    git-svn-id: https://enrich-ege.svn.sourceforge.net/svnroot/enrich-ege/project/oxgarage@77 4830b8e0-8a90-4283-b7f1-a7364ff6f0ab

--- tei-converter/src/main/java/pl/psnc/dl/ege/tei/Format.java
+++ tei-converter/src/main/java/pl/psnc/dl/ege/tei/Format.java
@@ -17,20 +17,20 @@ public enum Format {
 
 	RELAXNG("relaxng","application/xml-relaxng","relaxng","ODDC", "Compiled TEI ODD", "text", "RELAXNG schema created from ODD", "text", false, 10),
 
-	XHTML("xhtml","application/xhtml+xml","html","TEI", "TEI P5 XML", "text", "xHTML", "text", true, 9),
+	XHTML("xhtml","application/xhtml+xml","html","TEI", "TEI P5 XML Document", "text", "xHTML", "text", true, 9),
 
-	DOCX("docx","application/vnd.openxmlformats-officedocument.wordprocessingml.document","docx","TEI", "TEI P5 XML", "text", "Microsoft Word (.docx)", "text", true, 9),
+	DOCX("docx","application/vnd.openxmlformats-officedocument.wordprocessingml.document","docx","TEI", "TEI P5 XML Document", "text", "Microsoft Word (.docx)", "text", true, 9),
 
-	ODT("odt","application/vnd.oasis.opendocument.text","odt","TEI", "TEI P5 XML", "text", "Open Office Text (.odt)", "text", true, 8),
+	ODT("odt","application/vnd.oasis.opendocument.text","odt","TEI", "TEI P5 XML Document", "text", "OpenOffice Text (.odt)", "text", true, 8),
 
-	LATEX("latex","application/x-latex","latex","TEI", "TEI P5 XML", "text", "LaTeX", "text", true, 10),
+	LATEX("latex","application/x-latex","latex","TEI", "TEI P5 XML Document", "text", "LaTeX", "text", true, 10),
 	
-	FO("fo","application/xslfo+xml","fo","TEI", "TEI P5 XML", "text", "XSL-FO", "text", true, 10),
+	FO("fo","application/xslfo+xml","fo","TEI", "TEI P5 XML Document", "text", "XSL-FO", "text", true, 10),
 
-	EPUB("epub","application/epub+zip","epub","TEI", "TEI P5 XML", "text", "ePub", "text", true, 9),
+	EPUB("epub","application/epub+zip","epub","TEI", "TEI P5 XML Document", "text", "ePub", "text", true, 9),
 
-	TEXT("txt","text/plain","txt","TEI", "TEI P5 XML", "text", "Plain text", "text", true, 5),
-	RDF("rdf","application/rdf+xml","rdf","TEI", "TEI P5 XML", "text", "RDF XML", "text", true, 5);
+	TEXT("txt","text/plain","txt","TEI", "TEI P5 XML Document", "text", "Plain text", "text", true, 5),
+	RDF("rdf","application/rdf+xml","rdf","TEI", "TEI P5 XML Document", "text", "RDF XML", "text", true, 5);
 
 
 
